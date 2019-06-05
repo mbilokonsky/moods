012f3a73042896fdde2e59e47ccc8ceba496500b
myk bilokonsky <mbilokonsky@gmail.com>
2019-06-05 00:59:41 -0400

    short and sweet

---
+++ index.js
@@ -0,0 +1,11 @@
+const MOODS = {
+	GREAT: 		{ emoji: '😁', label: 'great' },
+	GOOD: 		{ emoji: '😀', label: 'good' },
+	OK: 			{ emoji: '🙂', label: 'ok' },
+	BLANK: 		{ emoji: '😐', label: 'blank' },
+	MEH: 			{ emoji: '😕', label: 'meh'	},
+	BAD: 			{ emoji: '😬', label: 'bad'	},
+	HORRIBLE: { emoji: '😩', label: 'horrible' }
+}
+
+module.exports = MOODS
